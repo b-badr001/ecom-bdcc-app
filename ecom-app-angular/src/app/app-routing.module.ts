a05c1ac60889bdc081c37cabe97518839e873329
b-badr001 <b_ajlil@etu.enset-media.ac.ma>
2024-03-04 11:24:36 +0100

    FC

--- ecom-app-angular/src/app/app-routing.module.ts
+++ ecom-app-angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductsComponent} from "./ui/products/products.component";
+import {CustomersComponent} from "./ui/customers/customers.component";
+import {AuthGuard} from "./guards/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "products", component : ProductsComponent, canActivate : [AuthGuard], data : {roles :['ADMIN']}},
+  {path : "customers", component : CustomersComponent, canActivate : [AuthGuard], data : {roles :['USER']}}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
